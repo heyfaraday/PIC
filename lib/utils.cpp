51e48019dea9cc47dad37f5dca3a44b195952f75
Ivan Edigaryev <heyfaraday@gmail.com>
2017-03-16 21:57:31 +0300

    utils added

---
+++ lib/utils.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "math.h"
+
+#include "utils.hpp"
+
+void vectorCross(std::vector<double>& a, std::vector<double>& b,
+                 std::vector<double>& result) {
+    result.at(1) = a.at(2) * b.at(3) - a.at(3) * b.at(2);
+    result.at(2) = a.at(3) * b.at(1) - a.at(1) * b.at(3);
+    result.at(3) = a.at(1) * b.at(2) - a.at(2) * b.at(1);
+}
+
+void updateX(std::vector<double>& v, std::vector<double>& x, double dt) {
+    x.at(1) += v.at(1) * dt;
+    x.at(2) += v.at(2) * dt;
+    x.at(3) += v.at(3) * dt;
+}
+
+void updateGamma(std::vector<double>& v, double& gamma) {
+    gamma = sqrt(1 - v.at(1) * v.at(1) - v.at(2) * v.at(2) - v.at(3) * v.at(3));
+}
---
+++ lib/utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+void vectorCross(std::vector<double>& a, std::vector<double>& b, std::vector<double>& result);
+
+void updateX(std::vector<double>& v, std::vector<double>& x, double dt);
+
+void updateGamma(std::vector<double>& v, double& gamma);
