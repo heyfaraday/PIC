89f105041a80a2799cb222685abfb4bbdf2e8116
Ivan Edigaryev <heyfaraday@gmail.com>
2017-03-16 22:06:17 +0300

    parameters fix

--- lib/parameters.hpp
+++ lib/parameters.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-double read_from_file (const std::string param);
+double read_from_file(const std::string param);
+
+extern const double dt_input;
+extern const double t_start_input;
+extern const double t_end_input;
+extern const double chargeOverMass_input;
 
 extern const std::vector<double> B_start;
 extern const std::vector<double> E_start;
 
 extern const std::vector<double> X_start;
 extern const std::vector<double> U_start;
-
-extern const double dt_input;
-extern const double t_start_input;
-extern const double t_end_input;
-extern const double chargeOverMass_input;
