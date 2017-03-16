c621e3b1a674a272679f87b16fc6ad09000e5761
Ivan Edigaryev <heyfaraday@gmail.com>
2017-03-16 22:11:22 +0300

    field and CMake fix

--- lib/field.cpp
+++ lib/field.cpp
@@ -1,8 +1,10 @@
-#import <vector>
+#include <vector>
+#include "math.h"
 
-#import "math.h"
-#import "parameters.hpp"
-#import "constants.hpp"
+#include "parameters.hpp"
+#include "constants.hpp"
+
+#include "field.hpp"
 
 void updateE(std::vector<double>& E, std::vector<double>& x, std::vector<double>& u, double t, double dt) {
 
@@ -16,7 +18,6 @@ void updateB(std::vector<double>& B, std::vector<double>& x, std::vector<double>
 
     B.at(1) = B.at(1);
     B.at(2) = B.at(2);
-    // B.at(3) = 20.0 - 0.005 * fabs(x.at(1) + x.at(2)); Drift
     B.at(3) = B.at(3);
 
 }
